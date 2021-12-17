6237b658eaa3d971696db302c8e476fad5f76116
Kaluub <kaluubs@gmail.com>
2021-12-16 23:08:02 -0500

    changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,15 @@
 function randomFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 };
+
+function swapElementVisibility(id, options) {
+    let element = document.getElementById(id);
+    element.hidden = !element.hidden;
+    if(options) {
+        let base = document.getElementById(options.id);
+        if(element.hidden) base.innerHTML = options.true;
+        else base.innerHTML = options.false;
+    }
+};
+
+export {randomFromArray, swapElementVisibility};
